061a90d9f96b55c6b85c098d1f58e01f5a6d2654
SKeshava_flexera <SKeshava@revenera.com>
2025-03-30 16:51:13 +0530

    push

--- catalog-service/src/main/java/com/sandeep/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sandeep/bookstore/catalog/CatalogServiceApplication.java
@@ -1,9 +1,13 @@
 package com.sandeep.bookstore.catalog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(ApplicationProperties.class)
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
---
+++ catalog-service/src/main/java/com/sandeep/bookstore/catalog/domain/ProductService.java
@@ -0,0 +1,54 @@
+package com.sandeep.bookstore.catalog.domain;
+
+import com.sandeep.bookstore.catalog.ApplicationProperties;
+import jakarta.transaction.Transactional;
+import org.aspectj.apache.bcel.classfile.Module;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ApplicationProperties applicationProperties;
+
+    ProductService(ProductRepository productRepository, ApplicationProperties applicationProperties) {
+        this.productRepository = productRepository;
+        this.applicationProperties = applicationProperties;
+    }
+
+    public List<Product> getProducts1(int pageNo) {
+        /*Pageable pageable = PageRequest.of(pageNo, applicationProperties.pageSize());
+        return productRepository.findAll(pageable);*/
+
+        return productRepository.findAll().stream().map(ProductMapper::toProduct).collect(Collectors.toList());
+    }
+
+    public PagedResult<Product> getProducts(int pageNo) {
+        Sort sort = Sort.by("name").ascending();
+        pageNo = pageNo <= 1 ? 0 : pageNo - 1;
+        Pageable pageable = PageRequest.of(pageNo, applicationProperties.pageSize(), sort);
+        Page<Product> productsPage = productRepository.findAll(pageable).map(ProductMapper::toProduct);
+
+        return new PagedResult<>(
+                productsPage.getContent(),
+                productsPage.getTotalElements(),
+                productsPage.getNumber() + 1,
+                productsPage.getTotalPages(),
+                productsPage.isFirst(),
+                productsPage.isLast(),
+                productsPage.hasNext(),
+                productsPage.hasPrevious());
+    }
+
+    public Optional<Product> getProductByCode(String code) {
+        return productRepository.findByCode(code).map(ProductMapper::toProduct);
+    }
+}
